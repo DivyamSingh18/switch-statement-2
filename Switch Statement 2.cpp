c5faa4f13e8fc598872c59acc2392070835410d9
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2020-09-03 18:06:14 +0530

    Create Switch Statement 2.cpp

---
+++ Switch Statement 2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+
+    cout<< "Enter the value of i between 1 to 4" << endl;
+    cin>> i;
+    switch(i)
+            {
+            case 1:
+                    cout<< "i contains 1";
+                    break;
+             case 2:
+                    cout<< "i contains 2";
+                    break;
+             case 3:
+                    cout<< "i contains 3";
+                    break;
+             case 4:
+                    cout<< "i contains 4";
+                    break;
+            }
+return 0;
+}
